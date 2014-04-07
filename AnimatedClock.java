23e954158ec16e543c8de7537e596fb4793ccf11
Matthew Boyette <Dyndrilliac@gmail.com>
2014-04-06 20:11:46 -0400

    Updated code formatting. Added binary download.

--- AnimatedClock.java
+++ AnimatedClock.java
@@ -6,8 +6,10 @@
 	This program displays an animated clock.
 */
 
-import api.gui.*;
-import api.util.*;
+import api.gui.ApplicationWindow;
+import api.gui.Clock;
+import api.util.EventHandler;
+import api.util.Support;
 
 import java.awt.Container;
 import java.awt.Dimension;
@@ -17,18 +19,22 @@ import javax.swing.JOptionPane;
 
 public class AnimatedClock
 {
-	private static boolean debugMode = false;
-	
-	public static final void main(final String[] args)
+	public final static void main(final String[] args)
 	{
-		ApplicationWindow mainWindow = null;
-		int               choice     = Support.promptDebugMode(mainWindow);
-		
-		debugMode = (choice == JOptionPane.YES_OPTION);
+		new AnimatedClock();
+	}
+	
+	private boolean				isDebugging	= false;
+	private ApplicationWindow	window		= null;
+	
+	public AnimatedClock()
+	{
+		this.setDebugging((Support.promptDebugMode(this.getWindow()) == JOptionPane.YES_OPTION));
 		
 		// Define a self-contained interface construction event handler.
-		EventHandler myDrawGUI = new EventHandler()
+		EventHandler myDrawGUI = new EventHandler(this)
 		{
+			@Override
 			public final void run(final Object... arguments) throws IllegalArgumentException
 			{
 				if (arguments.length <= 0)
@@ -40,9 +46,9 @@ public class AnimatedClock
 					throw new IllegalArgumentException("myDrawGUI Error : argument[0] is of incorrect type.");
 				}
 				
-				ApplicationWindow window      = (ApplicationWindow)arguments[0];
-				Container         contentPane = window.getContentPane();
-				Clock             clock       = new Clock(true);
+				ApplicationWindow	window		= (ApplicationWindow)arguments[0];
+				Container			contentPane	= window.getContentPane();
+				Clock				clock		= new Clock(true);
 				
 				contentPane.setLayout(new FlowLayout());
 				contentPane.add(clock);
@@ -50,9 +56,27 @@ public class AnimatedClock
 			}
 		};
 		
-		mainWindow = new ApplicationWindow(null, "Animated Clock Application", new Dimension(300, 300), debugMode, false, 
-			null, myDrawGUI);
-		
-		mainWindow.setIconImageByResourceName("icon.png");
+		this.setWindow(new ApplicationWindow(null, "Animated Clock Application", new Dimension(300, 300), this.isDebugging(), false, null, myDrawGUI));
+		this.getWindow().setIconImageByResourceName("icon.png");
+	}
+	
+	public final ApplicationWindow getWindow()
+	{
+		return this.window;
+	}
+	
+	public final boolean isDebugging()
+	{
+		return this.isDebugging;
+	}
+	
+	protected final void setDebugging(final boolean isDebugging)
+	{
+		this.isDebugging = isDebugging;
+	}
+	
+	protected final void setWindow(final ApplicationWindow window)
+	{
+		this.window = window;
 	}
 }
--- README.md
+++ README.md
@@ -9,3 +9,5 @@
 This code makes use of my [Custom Java API](https://github.com/Dyndrilliac/java-custom-api). In order to build this source, you should clone the repository for the API using your Git client, then import the project into your IDE of choice (I prefer Eclipse), and finally modify the build path to include the API project. For more detailed instructions, see the README for the API project.
 
 This program displays an animated analogue style clock.
+
+A pre-compiled JAR binary can be downloaded from [this link](https://www.dropbox.com/s/fe6pki228mwleau/AnimatedClock.jar).
