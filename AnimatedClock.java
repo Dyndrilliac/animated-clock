d5b4d63c820876e81ec7c05969993ca26984ab77
Matthew Boyette <Dyndrilliac@gmail.com>
2015-02-19 11:53:32 -0500

    Automated cleanup / refactoring.

--- AnimatedClock.java
+++ AnimatedClock.java
@@ -1,70 +1,71 @@
 /*
-	Title:  AnimatedClock
-	Author: Matthew Boyette
-	Date:   3/25/2013
-	
-	This program displays an animated clock.
-*/
+ * Title: AnimatedClock
+ * Author: Matthew Boyette
+ * Date: 3/25/2013
+ *
+ * This program displays an animated clock.
+ */
+
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 import api.gui.ApplicationWindow;
 import api.gui.Clock;
 import api.util.EventHandler;
 import api.util.Support;
 
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-
 public class AnimatedClock
-{	
+{
 	public final static void main(final String[] args)
 	{
 		new AnimatedClock(args);
 	}
-	
+
 	private boolean				isDebugging	= false;
 	private ApplicationWindow	window		= null;
-	
+
 	public AnimatedClock(final String[] args)
 	{
 		this.setDebugging(Support.promptDebugMode(this.getWindow()));
-		
+
 		// Define a self-contained interface construction event handler.
 		EventHandler<AnimatedClock> myDrawGUI = new EventHandler<AnimatedClock>(this)
-		{
-			private final static long serialVersionUID = 1L;
+			{
+			private final static long	serialVersionUID	= 1L;
 
 			@Override
 			public final void run(final ApplicationWindow window)
 			{
-				Container	contentPane	= window.getContentPane();
-				Clock		clock		= new Clock(true);
-				
+				Container contentPane = window.getContentPane();
+				Clock clock = new Clock(true);
+
 				contentPane.setLayout(new FlowLayout());
 				contentPane.add(clock);
 				clock.startAnimation();
 			}
-		};
-		
-		this.setWindow(new ApplicationWindow(null, "Animated Clock Application", new Dimension(350, 300), this.isDebugging(), false, null, myDrawGUI));
-		this.getWindow().setIconImageByResourceName("icon.png");
+			};
+
+			this.setWindow(new ApplicationWindow(null, "Animated Clock Application", new Dimension(350, 300), this.isDebugging(),
+				false, null, myDrawGUI));
+			this.getWindow().setIconImageByResourceName("icon.png");
 	}
-	
+
 	public final ApplicationWindow getWindow()
 	{
 		return this.window;
 	}
-	
+
 	public final boolean isDebugging()
 	{
 		return this.isDebugging;
 	}
-	
+
 	protected final void setDebugging(final boolean isDebugging)
 	{
 		this.isDebugging = isDebugging;
 	}
-	
+
 	protected final void setWindow(final ApplicationWindow window)
 	{
 		this.window = window;
